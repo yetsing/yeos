758bf9ef3038b428323dfc820595691898ba6722
yetsing <heopro@163.com>
2026-01-15 22:44:51 +0800

    Add font drawing functionality and improve project structure

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -7,69 +7,22 @@
 #include <cstdint>
 #include <cstddef>
 
+// #@@range_begin(includes)
 #include "frame_buffer_config.hpp"
+#include "graphics.hpp"
+#include "font.hpp"
+// #@@range_end(includes)
 
-struct PixelColor {
-  uint8_t r, g, b;
-};
-
-// #@@range_begin(pixel_writer)
-class PixelWriter {
- public:
-  PixelWriter(const FrameBufferConfig& config) : config_{config} {
-  }
-  virtual ~PixelWriter() = default;
-  virtual void Write(int x, int y, const PixelColor& c) = 0;
-
- protected:
-  uint8_t* PixelAt(int x, int y) {
-    return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
-  }
-
- private:
-  const FrameBufferConfig& config_;
-};
-// #@@range_end(pixel_writer)
-
-// #@@range_begin(derived_pixel_writer)
-class RGBResv8BitPerColorPixelWriter : public PixelWriter {
- public:
-  using PixelWriter::PixelWriter;
-
-  virtual void Write(int x, int y, const PixelColor& c) override {
-    auto p = PixelAt(x, y);
-    p[0] = c.r;
-    p[1] = c.g;
-    p[2] = c.b;
-  }
-};
-
-class BGRResv8BitPerColorPixelWriter : public PixelWriter {
- public:
-  using PixelWriter::PixelWriter;
-
-  virtual void Write(int x, int y, const PixelColor& c) override {
-    auto p = PixelAt(x, y);
-    p[0] = c.b;
-    p[1] = c.g;
-    p[2] = c.r;
-  }
-};
-// #@@range_end(derived_pixel_writer)
-
-// #@@range_begin(placement_new)
 void* operator new(size_t size, void* buf) {
   return buf;
 }
 
 void operator delete(void* obj) noexcept {
 }
-// #@@range_end(placement_new)
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
-// #@@range_begin(call_pixel_writer)
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
@@ -92,6 +45,8 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
+
+  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+  WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
   while (1) __asm__("hlt");
 }
-// #@@range_end(call_pixel_writer)
