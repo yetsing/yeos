7ae674e5111f424d22513312a221f734ab500dca
yetsing <heopro@163.com>
2026-01-25 23:49:26 +0800

    Add keyboard support: implement keycode mapping and initialize keyboard handling

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -32,6 +32,7 @@
 #include "message.hpp"
 #include "timer.hpp"
 #include "acpi.hpp"
+#include "keyboard.hpp"
 
 int printk(const char* format, ...) {
   va_list ap;
@@ -95,8 +96,9 @@ extern "C" void KernelMainNewStack(
   acpi::Initialize(acpi_table);
   InitializeLAPICTimer(*main_queue);
 
-  timer_manager->AddTimer(Timer(200, 2));
-  timer_manager->AddTimer(Timer(600, -1));
+  // #@@range_begin(call_initkb)
+  InitializeKeyboard(*main_queue);
+  // #@@range_end(call_initkb)
 
   char str[128];
 
@@ -125,13 +127,14 @@ extern "C" void KernelMainNewStack(
       usb::xhci::ProcessEvents();
       break;
     case Message::kTimerTimeout:
-      printk("Timer: timeout = %lu, value = %d\n",
-          msg.arg.timer.timeout, msg.arg.timer.value);
-      if (msg.arg.timer.value > 0) {
-        timer_manager->AddTimer(Timer(
-            msg.arg.timer.timeout + 100, msg.arg.timer.value + 1));
+      break;
+    // #@@range_begin(event_handling)
+    case Message::kKeyPush:
+      if (msg.arg.keyboard.ascii != 0) {
+        printk("%c", msg.arg.keyboard.ascii);
       }
       break;
+    // #@@range_end(event_handling)
     default:
       Log(kError, "Unknown message type: %d\n", msg.type);
     }
