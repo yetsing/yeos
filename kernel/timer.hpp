93b8651d22ad220c02bc4414bb1849f8d34e77a4
yetsing <heopro@163.com>
2026-01-26 23:58:46 +0800

    Implement task management: add task context switching and timer integration

--- kernel/timer.hpp
+++ kernel/timer.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <queue>
 #include <vector>
+#include <limits>
 #include "message.hpp"
 
 void InitializeLAPICTimer(std::deque<Message>& msg_queue);
@@ -30,7 +31,7 @@ class TimerManager {
  public:
   TimerManager(std::deque<Message>& msg_queue);
   void AddTimer(const Timer& timer);
-  void Tick();
+  bool Tick();
   unsigned long CurrentTick() const { return tick_; }
 
  private:
@@ -42,5 +43,10 @@ class TimerManager {
 extern TimerManager* timer_manager;
 extern unsigned long lapic_timer_freq;
 const int kTimerFreq = 100;
+
+// #@@range_begin(tasktimer)
+const int kTaskTimerPeriod = static_cast<int>(kTimerFreq * 0.02);
+const int kTaskTimerValue = std::numeric_limits<int>::min();
+// #@@range_end(tasktimer)
 
 void LAPICTimerOnInterrupt();
